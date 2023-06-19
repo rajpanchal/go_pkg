986ab7d7be502e1a5566ff20b2bc2c4a1d313681
Abhishek Patwardhan <abhishek.patwardhan@nutanix.com>
2023-06-19 17:34:12 +0000

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Abhishekntnx/abhishek_go_mod
+
+go 1.17
+
+require github.com/gofrs/uuid v4.4.0+incompatible
---
+++ main.go
@@ -0,0 +1,11 @@
+package abhishekgomod
+
+import (
+	"fmt"
+
+	_ "github.com/gofrs/uuid"
+)
+
+func PrintHello() {
+	fmt.Println("Package to test custom module update")
+}
